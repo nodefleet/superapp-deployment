c68ed25101545670e050829a436f9f269f92a697
Andrew Nguyen <andrew@ginchu.com>
2025-07-14 11:00:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-docker build -t canopy --build-arg 'BRANCH=beta-0.0.1' --build-arg BUILD_PATH=cmd/cli  ./docker_image/ && \
+docker build -t canopy --build-arg 'BRANCH=beta-0.1.0' --build-arg BUILD_PATH=cmd/cli  ./docker_image/ && \
 docker run --user root -it -p 50000:50000 -p 50001:50001 -p 50002:50002 -p 50003:50003 -p 9001:9001 --name canopy-config  --volume ${PWD}/canopy_data/node1/:/root/.canopy/ canopy && \
 docker stop canopy-config && docker rm canopy-config && \
 cp canopy_data/node1/validator_key.json canopy_data/node2/ && \
