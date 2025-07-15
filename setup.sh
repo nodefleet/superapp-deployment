bb2ced6b9984c6f3b524a8d3db71682a90d12da4
Roniel Valdez <roniel_valdez@outlook.com>
2025-07-15 19:51:58 +0200

    feat: additional replacements, remove replacement from entrypoint.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo "setting up the validator key"
 docker build -t canopy --build-arg 'BRANCH=beta-0.1.3' --build-arg BUILD_PATH=cmd/cli  ./docker_image/ && \
 docker run --user root -it -p 50000:50000 -p 50001:50001 -p 50002:50002 -p 50003:50003 -p 9001:9001 --name canopy-config  --volume ${PWD}/canopy_data/node1/:/root/.canopy/ canopy && \
@@ -32,39 +32,75 @@ else
 fi
 
 # only ask for domain and email if setup type is "full"
+
+# ask user for domain input
+read -p "Please enter the domain [optional for simple setup]: " DOMAIN
+
 if [[ "$SETUP_TYPE" == "full" ]]; then
-    # ask user for domain input
-    read -p "Please enter the domain: " DOMAIN
+  # validate that domain is not empty for full setup
+  while [[ -z "$DOMAIN" ]]; do
+      echo "Domain cannot be empty."
+      read -p "Please enter the domain: " DOMAIN
+  done
+fi
 
-    # validate that domain is not empty
-    while [[ -z "$DOMAIN" ]]; do
-        echo "Domain cannot be empty."
-        read -p "Please enter the domain: " DOMAIN
-    done
+# ask user for acme email input
+read -p "Please enter email to validate the domain against [optional for simple setup]: " ACME_EMAIL
 
-    # ask user for acme email input
-    read -p "Please enter email to validate the domain against: " ACME_EMAIL
+if [[ "$SETUP_TYPE" == "full" ]]; then
+  # validate that email is not empty for full setup
+  while [[ -z "$ACME_EMAIL" ]]; do
+      echo "email cannot be empty."
+      read -p "Please enter the email: " ACME_EMAIL
+  done
+fi
 
-    # validate that email is not empty
-    while [[ -z "$ACME_EMAIL" ]]; do
-        echo "email cannot be empty."
-        read -p "Please enter the email: " ACME_EMAIL
-    done
+# define the path to the template and new .env file
+ENV_TEMPLATE_FILE="$STACK_PATH/.env.template"
+ENV_FILE="$STACK_PATH/.env"
 
-    # define the path to the template and new .env file
-    ENV_TEMPLATE_FILE="$STACK_PATH/.env.template"
-    ENV_FILE="$STACK_PATH/.env"
+# check if .env.template file exists
+if [[ ! -f "$ENV_TEMPLATE_FILE" ]]; then
+  echo ".env.template file not found, please create it with the default values from the repository."
+    exit 1
+fi
 
-    # check if .env.template file exists
-    if [[ ! -f "$ENV_TEMPLATE_FILE" ]]; then
-      echo ".env.template file not found, please create it with the default values from the repository."
-        exit 1
-    fi
+# perform sed substitution and create new .env file
+sed -e "s/DOMAIN=.*/DOMAIN=$DOMAIN/" -e "s/ACME_EMAIL=.*/ACME_EMAIL=$ACME_EMAIL/" "$ENV_TEMPLATE_FILE" > "$ENV_FILE"
+echo "Created .env file with domain: $DOMAIN and email: $ACME_EMAIL"
 
-    # perform sed substitution and create new .env file
-    sed -e "s/DOMAIN=.*/DOMAIN=$DOMAIN/" -e "s/ACME_EMAIL=.*/ACME_EMAIL=$ACME_EMAIL/" "$ENV_TEMPLATE_FILE" > "$ENV_FILE"
+# perform the sed substitution for the traefik.yml
+if [[ -n "$ACME_EMAIL" ]]; then
+  TRAEFIK_PATH="$STACK_PATH/loadbalancer/traefik.yml"
+  if grep -q "\${ACME_EMAIL}" "$TRAEFIK_PATH"; then
+    echo "Replacing \${ACME_EMAIL} with $ACME_EMAIL in $TRAEFIK_PATH"
+    sed -i '' "s|\${ACME_EMAIL}|$ACME_EMAIL|g" "$TRAEFIK_PATH"
+  else
+    echo "ACME_EMAIL already set."
+  fi
+fi
 
-    echo "Created .env file with domain: $DOMAIN and email: $ACME_EMAIL"
+NODE1_CONFIG="$(realpath "$(dirname "$0")/canopy_data/node1/config.json")"
+NODE2_CONFIG="$(realpath "$(dirname "$0")/canopy_data/node2/config.json")"
+
+if [[ -n "$DOMAIN" ]]; then
+  # perform the sed substitution for the node-1 config.json
+  if grep -q "tcp://node1.localhost" "$NODE1_CONFIG"; then
+    echo "Replacing localhost with $DOMAIN in $NODE1_CONFIG"
+    sed -i '' "s|tcp://node1.localhost|tcp://node1.$DOMAIN|g" "$NODE1_CONFIG"
+  else
+    echo "localhost already replaced with $DOMAIN."
+  fi
+fi
+
+if [[ -n "$DOMAIN" ]]; then
+  # perform the sed substitution for the node-2 config.json
+  if grep -q "tcp://node2.localhost" "$NODE2_CONFIG"; then
+    echo "Replacing localhost with $DOMAIN in $NODE2_CONFIG"
+    sed -i '' "s|tcp://node2.localhost|tcp://node2.$DOMAIN|g" "$NODE2_CONFIG"
+  else
+    echo "localhost already replaced with $DOMAIN."
+  fi
 fi
 
 echo "setup complete ✅"
